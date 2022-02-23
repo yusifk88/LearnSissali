2e04bbeb68811b0f452ef1a63fbb1562391a8f8a
yusifk88 <amloyal4life@gmail.com>
2022-02-23 06:16:58 +0000

    first commit, menu layout

---
+++ src/components/app.vue
@@ -0,0 +1,79 @@
+<template>
+  <f7-app v-bind="f7params" >
+
+  <!-- Left panel with cover effect when hidden -->
+  <f7-panel left cover :visible-breakpoint="960">
+    <f7-view>
+      <f7-page>
+        <f7-navbar title="Left Panel"></f7-navbar>
+        <f7-block-title>Left View Navigation</f7-block-title>
+        <f7-list>
+          <f7-list-item link="/left-page-1/" title="Left Page 1"></f7-list-item>
+          <f7-list-item link="/left-page-2/" title="Left Page 2"></f7-list-item>
+        </f7-list>
+        <f7-block-title>Control Main View</f7-block-title>
+        <f7-list>
+          <f7-list-item link="/about/" view=".view-main" panel-close title="About"></f7-list-item>
+          <f7-list-item link="/form/" view=".view-main" panel-close title="Form"></f7-list-item>
+          <f7-list-item link="#" view=".view-main" back panel-close title="Back in history"></f7-list-item>
+        </f7-list>
+      </f7-page>
+    </f7-view>
+  </f7-panel>
+
+
+  <!-- Your main view, should have "view-main" class -->
+  <f7-view main class="safe-areas" url="/"></f7-view>
+
+
+  </f7-app>
+</template>
+<script>
+  import { ref, onMounted } from 'vue';
+  import { f7, f7ready } from 'framework7-vue';
+
+
+  import routes from '../js/routes.js';
+  import store from '../js/store';
+
+  export default {
+    setup() {
+
+      // Framework7 Parameters
+      const f7params = {
+        name: 'Lean Sissali', // App name
+        theme: 'auto', // Automatic theme detection
+
+
+
+        // App store
+        store: store,
+        // App routes
+        routes: routes,
+      };
+      // Login screen data
+      const username = ref('');
+      const password = ref('');
+
+      const alertLoginData = () => {
+        f7.dialog.alert('Username: ' + username.value + '<br>Password: ' + password.value, () => {
+          f7.loginScreen.close();
+        });
+      }
+      onMounted(() => {
+        f7ready(() => {
+
+
+          // Call F7 APIs here
+        });
+      });
+
+      return {
+        f7params,
+        username,
+        password,
+        alertLoginData
+      }
+    }
+  }
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,158 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar large :sliding="true" no-shadow>
+      <f7-nav-left>
+        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title sliding>Learn Sissali</f7-nav-title>
+
+      <f7-nav-title-large>Learn Sissali</f7-nav-title-large>
+      <f7-nav-right>
+        <f7-link class="searchbar-enable" data-searchbar=".searchbar-demo" icon-ios="f7:search" icon-aurora="f7:search" icon-md="material:search"></f7-link>
+      </f7-nav-right>
+
+      <f7-searchbar
+          class="searchbar-demo"
+          expandable
+          search-container=".search-list"
+          search-in=".item-title"
+          :disable-button="!theme.aurora"
+      ></f7-searchbar>
+
+    </f7-navbar>
+
+    <f7-block margin>
+
+    <f7-row>
+
+      <f7-col class="" width="30" v-for="menu in menus" :key="menu.route">
+        <div style="padding: 10px;">
+
+          <center>
+
+        <img :src="menu.icon" width="60"><br>
+          {{menu.title}}
+          </center>
+
+        </div>
+
+      </f7-col>
+
+    </f7-row>
+
+    </f7-block>
+
+  </f7-page>
+</template>
+<script>
+import { theme } from 'framework7-vue';
+
+  export default {
+
+    data(){
+      return{
+        theme,
+
+        menus:[
+          {
+            title:"Greetings",
+            icon:"/icons/handshake.png",
+            route:"/view/greetings"
+          },
+          {
+            title:"Conversation",
+            icon:"/icons/chat.png",
+            route:"/view/conversation"
+          },
+          {
+            title:"Numbers",
+            icon:"/icons/numbers.png",
+            route:"/view/numbers"
+          },
+          {
+            title:"Time & Date",
+            icon:"/icons/clock.png",
+            route:"/view/timedate"
+          },
+          {
+            title:"Direction & places",
+            icon:"/icons/map.png",
+            route:"/view/direction"
+          },
+          {
+            title:"Transport",
+            icon:"/icons/bus.png",
+            route:"/view/transportation"
+          },
+          {
+            title:"Hospitality",
+            icon:"/icons/house.png",
+            route:"/view/accommodation"
+          },
+          {
+            title:"Eating Out",
+            icon:"/icons/cutlery.png",
+            route:"/view/eating"
+          },
+          {
+            title:"Shopping",
+            icon:"/icons/shopping.png",
+            route:"/view/shopping"
+          },
+          {
+            title:"Colours",
+            icon:"/icons/colors.png",
+            route:"/view/colors"
+          },
+          {
+            title:"Cities",
+            icon:"/icons/city.png",
+            route:"/view/cities"
+          },
+          {
+            title:"Countries",
+            icon:"/icons/globe.png",
+            route:"/view/countries"
+          },
+          {
+            title:"Tourist Attractions",
+            icon:"/icons/gallery.png",
+            route:"/view/tourist"
+          },
+          {
+            title:"Family",
+            icon:"/icons/family.png",
+            route:"/view/family"
+          },
+          {
+            title:"Dating",
+            icon:"/icons/dating.png",
+            route:"/view/dating"
+          },
+          {
+            title:"Emergency",
+            icon:"/icons/alert.png",
+            route:"/view/emergency"
+          },
+          {
+            title:"Feeling Sick",
+            icon:"/icons/sick.png",
+            route:"/view/feelingsick"
+          },
+          {
+            title:"Tongue Twister",
+            icon:"/icons/rocket.png",
+            route:"/view/tongtwister"
+          }
+
+        ]
+
+      }
+    }
+
+
+  }
+
+
+</script>
