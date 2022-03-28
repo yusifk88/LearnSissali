19aeb127609b50435d00b5401173f1738672c9bd
yusifk88 <amloyal4life@gmail.com>
2022-03-28 01:54:47 +0000

    added more translations

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,15 +1,84 @@
 <template>
   <f7-page name="about">
     <f7-navbar no-shadow title="About" back-link="Back"></f7-navbar>
-    <f7-block-title>About My App</f7-block-title>
+    <f7-block-title>About Ln Sissali</f7-block-title>
     <f7-block strong>
-      <p>Fugiat perspiciatis excepturi, soluta quod non ullam deleniti. Nobis sint nemo consequuntur, fugiat. Eius perferendis animi autem incidunt vel quod tenetur nostrum, voluptate omnis quasi quidem illum consequuntur, a, quisquam.</p>
-      <p>Laudantium neque magnam vitae nemo quam commodi, in cum dolore obcaecati laborum, excepturi harum, optio qui, consequuntur? Obcaecati dolor sequi nesciunt culpa quia perspiciatis, reiciendis ex debitis, ut tenetur alias.</p>
+      <p>
+        "Ln Sissali" pronounced learn Sissali is a community-sourced project meant to simplify the learning curve of the sissili
+        dialect, the aim of this project is to make it easier to lookup words in English and find their sissali equivalents.
+      </p>
+      <h2>What it is.</h2>
+      <p>
+        Ln sissali is meant to be your guide to what an everyday word or phrase may mean in sissali, it is also meant to help complete non-speakers
+        easily make up conversations with native speakers.
+      </p>
+      <h2>What it is NOT</h2>
+
+      <p>
+        LN Sissali is not meant to be your go-to source for academic validations, we are committed to increasing it's accuracy but
+        as a community we do not recommend it as an academically certified source for the language/dialect.
+      </p>
+      <h2>Contributors/Team</h2>
+
+      <f7-card class="demo-card-header-pic" v-for="person in people" no-shadow>
+        <f7-card-header
+            class="no-border"
+            valign="bottom"
+            style="background-image:url(https://cdn.framework7.io/placeholder/people-1000x600-6.jpg)"
+        >{{person.name}}</f7-card-header>
+        <f7-card-content>
+          <p class="date">{{person.title}}</p>
+          <p>{{person.profession}}</p>
+        </f7-card-content>
+        <f7-card-footer>
+          <f7-link>Like</f7-link>
+          <f7-link>Read more</f7-link>
+        </f7-card-footer>
+      </f7-card>
     </f7-block>
     <f7-block>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni molestiae laudantium dignissimos est nobis delectus nemo ea alias voluptatum architecto, amet similique, saepe iste consectetur in repellat ut minus quibusdam!</p>
-      <p>Molestias et distinctio porro nesciunt ratione similique, magni doloribus, rerum nobis, aliquam quae reiciendis quasi modi. Nam a recusandae, fugiat in ea voluptates fuga eius, velit corrupti reprehenderit dignissimos consequatur!</p>
-      <p>Blanditiis, cumque quo adipisci. Molestiae, dolores dolorum quos doloremque ipsa ullam eligendi commodi deserunt doloribus inventore magni? Ea mollitia veniam nostrum nihil, iusto doloribus a at! Ea molestiae ullam delectus!</p>
-    </f7-block>
+
+
+         </f7-block>
   </f7-page>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      people:[
+        {
+          name:"Haruna Kuri",
+          title:"Project coordinator",
+          profession:"Professional Teacher"
+        },
+        {
+          name:"Emanuela Lieku",
+          title:"Translator",
+          profession:"Fashion designer"
+        },
+        {
+          name:"Yusif katulie",
+          title:"Developer",
+          profession:"Senior software engineer"
+        },
+
+      ]
+    }
+  }
+
+
+}
+</script>
+
+<style>
+.demo-card-header-pic .card-header {
+  height: 40vw;
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+}
+.demo-card-header-pic .card-content-padding .date{
+  color: #8e8e93;
+}
+</style>
